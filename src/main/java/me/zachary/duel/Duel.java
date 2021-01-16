8b87c936fbfcdc81241c0c9b38bc52d755e83e84
Zach_FR <zac232003@icloud.com>
2021-01-16 12:00:15 -0500

    Initial commit

---
+++ src/main/java/me/zachary/duel/Duel.java
@@ -0,0 +1,17 @@
+package me.zachary.duel;
+
+import me.zachary.zachcore.ZachCorePlugin;
+
+public final class Duel extends ZachCorePlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        preEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
